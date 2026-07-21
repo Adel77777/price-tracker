899bec98a28de987b897de7b5788d285c7329ac3
Adel <adeljhjh@gmail.com>
2026-07-21 13:05:00 +0100

    adding a config example file

---
+++ config.example.py
@@ -0,0 +1,7 @@
+PRODUCTS = []
+EMAIL_SENDER = "your_gmail@gmail.com"
+EMAIL_PASSWORD = "your_app_password"
+EMAIL_RECEIVER = "your_gmail@gmail.com"
+CHECK_INTERVAL = 3600
+TELEGRAM_TOKEN = "your_telegram_bot_token"
+TELEGRAM_CHAT_ID = "your_chat_id"
